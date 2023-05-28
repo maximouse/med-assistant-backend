cb61951ecbb36d8c482e8f0cd14de1185bb09e01
Maksim Martsenyuk <maximouse@users.noreply.github.com>
2023-05-28 20:54:36 +0300

    Authorization header added

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+const fs = require('fs')
 const PORT = process.env.PORT || 5000
-console.log(process.env.MONGODB)
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ["http://localhost:3000", "https://med-assistant-frontend-production.up.railway.app"],
     credentials: true,
-    allowedHeaders:['content-type']
+    allowedHeaders:['content-type, Authorization']
   });
   const config = new DocumentBuilder()
   .setTitle('Электронный помощник руководителя (Backend)')
@@ -19,6 +20,7 @@ async function bootstrap() {
   .build();
 
   const document = SwaggerModule.createDocument(app, config);
+  // fs.writeFileSync("./swagger-spec.json", JSON.stringify(document));
   SwaggerModule.setup('api', app, document);
 
   await app.listen(PORT, ()=>console.log(`Started at ${PORT}`))
